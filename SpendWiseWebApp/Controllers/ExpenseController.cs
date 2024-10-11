eacbf1bbfd5846f1fc9118acf1a097bce0af79ac
Yosef Giday <A00288051@mycambrian.ca>
2024-10-11 19:14:54 +0000

    Controller created

---
+++ SpendWiseWebApp/Controllers/ExpenseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+//using SpendWiseWebApp.Models;
+//using SpendWiseWebApp.Data;
+
+[Route("api/controller")]
+[ApiController]
+public class ExpenseController: ControllerBase{
+
+
+}
