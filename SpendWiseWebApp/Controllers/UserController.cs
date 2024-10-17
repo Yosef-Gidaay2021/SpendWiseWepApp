9c3d4a6bf544e80e930ed030b25eebb38a420123
Parampreet30 <A00264414@mycambrian.ca>
2024-10-17 23:43:58 +0000

    security added to controllers

--- SpendWiseWebApp/Controllers/UserController.cs
+++ SpendWiseWebApp/Controllers/UserController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SpendWiseWebApp.Data;
 using spendwisebase.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace SpendWiseWebApp.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
