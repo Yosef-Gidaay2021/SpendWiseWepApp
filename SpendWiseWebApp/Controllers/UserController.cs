73983b818fbea58cf637392c8b72d5432696ac9e
Parampreet30 <A00264414@mycambrian.ca>
2024-10-18 12:29:43 -0400

    fixed issue related to db colum

--- SpendWiseWebApp/Controllers/UserController.cs
+++ SpendWiseWebApp/Controllers/UserController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace SpendWiseWebApp.Controllers
 {
-    [Authorize]
+   
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
